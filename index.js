9160cb53834b465621d073bb7decd87f6ef1b831
cataopriscila <cataopriscila@gmail.com>
2022-01-22 11:16:46 -0300

    feat: implement API server

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const fruityvice= require('./api/fruityvice');
 const express = require('express');
 const cors = require('cors');
 
-
 const app = express();
 app.use(cors());
 app.use(express.json());
